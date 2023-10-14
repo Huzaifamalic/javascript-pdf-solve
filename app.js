7a3a659032165ddfd12302f82b1e6385b494a709
Huzaifamalic <135388170+Huzaifamalic@users.noreply.github.com>
2023-10-15 01:15:32 +0500

    done

---
+++ app.js
@@ -0,0 +1,25 @@
+// Alert
+
+
+//Q1
+
+alert("entre your corrert pasword" )  
+ 
+// Q 3
+
+alert("well come to js land " +"\n"  +  "Happycoding")  
+
+// Q  4 
+
+// varibale string / number
+ 
+
+
+// Q  1 to 6
+
+let FirstName = "Huzaifa \n" ;
+let lastname = "malick \n" ;
+let age = "22 \n";
+let Email = "huzaifamalick506@gmail \n"
+let BirthDate= 7 + "july" + 2001
+alert( FirstName+ lastname + age + Email + BirthDate);
